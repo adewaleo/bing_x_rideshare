d1ab1dc7a0ffff9e0152591794820606a2689e27
Derek Bekoe <debekoe@microsoft.com>
2019-07-22 22:59:37 +0000

    some clean up

--- frontend/src/views/IndexSections/Hero.jsx
+++ frontend/src/views/IndexSections/Hero.jsx
@@ -65,21 +65,6 @@ class Hero extends React.Component {
                         </span>
                         <span className="btn-inner--text">Get Started</span>
                       </Button>{" "}
-                      {/* <Button
-                        className="btn-icon mb-3 mb-sm-0"
-                        color="github"
-                        href="https://github.com/creativetimofficial/argon-design-system-react"
-                        size="lg"
-                        target="_blank"
-                      >
-                        <span className="btn-inner--icon mr-1">
-                          <i className="fa fa-github" />
-                        </span>
-                        <span className="btn-inner--text">
-                          <span className="text-warning mr-1">Star us</span>
-                          on Github
-                        </span>
-                      </Button> */}
                     </div>
                     <div className="mt-5">
                       <small className="text-white font-weight-bold mb-0 mr-2">
