2a2f160bac5b2a51cdc4545b7e34eb8e338008f2
Janvi Ketan Jatakia <jatakiajanvi12@gmail.com>
2019-07-23 16:44:35 -0700

    Added the recommendations dummy in the app.py

--- backend/app.py
+++ backend/app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_restful import Api
 
 from resources.todo import Todo, TodoList
-from resources.endpoints import PlaceAutocomplete
+from resources.endpoints import PlaceAutocomplete, Recommendations
 
 app = Flask(__name__)
 api = Api(app)
@@ -13,6 +13,7 @@ api.add_resource(TodoList, '/todos')
 api.add_resource(Todo, '/todos/<todo_id>')
 
 api.add_resource(PlaceAutocomplete, '/place_autocomplete/<query>')
+api.add_resource(Recommendations, '/recommendations')
 
 
 if __name__ == '__main__':
