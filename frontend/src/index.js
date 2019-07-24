df8d4b0a11a2aa7263a5033f6967109a0604a452
Ryan Efendy <ryanefendy95@gmail.com>
2019-07-23 17:42:41 -0700

    add eslint & prettier, fix linting

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -15,36 +15,36 @@
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
 */
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 
-import "assets/vendor/nucleo/css/nucleo.css";
-import "assets/vendor/font-awesome/css/font-awesome.min.css";
-import "assets/scss/argon-design-system-react.scss";
+import 'assets/vendor/nucleo/css/nucleo.css';
+import 'assets/vendor/font-awesome/css/font-awesome.min.css';
+import 'assets/scss/argon-design-system-react.scss';
 
-import Index from "views/Index.jsx";
-import About from "views/About.jsx";
-import Recommendations from "views/Recommendations.jsx";
-import Components from "views/Components.jsx";
-import Landing from "views/examples/Landing.jsx";
-import Login from "views/examples/Login.jsx";
-import Profile from "views/examples/Profile.jsx";
-import Register from "views/examples/Register.jsx";
+import Index from 'views/Index.jsx';
+import About from 'views/About.jsx';
+import Recommendations from 'views/Recommendations.jsx';
+import Components from 'views/Components.jsx';
+import Landing from 'views/examples/Landing.jsx';
+import Login from 'views/examples/Login.jsx';
+import Profile from 'views/examples/Profile.jsx';
+import Register from 'views/examples/Register.jsx';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <Switch>
-      <Route path="/" exact render={props => <Index {...props} />} />
-      <Route path="/about" exact render={props => <About {...props} />} />
-      <Route path="/recommendations" exact render={props => <Recommendations {...props} />} />
-      <Route path="/components" exact render={props => <Components {...props} />} />
-      <Route path="/landing-page" exact render={props => <Landing {...props} />} />
-      <Route path="/login-page" exact render={props => <Login {...props} />} />
-      <Route path="/profile-page" exact render={props => <Profile {...props} />} />
-      <Route path="/register-page" exact render={props => <Register {...props} />} />
-      <Redirect to="/" />
-    </Switch>
-  </BrowserRouter>,
-  document.getElementById("root")
+        <BrowserRouter>
+                <Switch>
+                        <Route path="/" exact render={props => <Index {...props} />} />
+                        <Route path="/about" exact render={props => <About {...props} />} />
+                        <Route path="/recommendations" exact render={props => <Recommendations {...props} />} />
+                        <Route path="/components" exact render={props => <Components {...props} />} />
+                        <Route path="/landing-page" exact render={props => <Landing {...props} />} />
+                        <Route path="/login-page" exact render={props => <Login {...props} />} />
+                        <Route path="/profile-page" exact render={props => <Profile {...props} />} />
+                        <Route path="/register-page" exact render={props => <Register {...props} />} />
+                        <Redirect to="/" />
+                </Switch>
+        </BrowserRouter>,
+        document.getElementById('root')
 );
