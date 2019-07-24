20ec55338f0667196a8916e49af3e60d3e3c9093
Ryan Efendy <ryanefendy95@gmail.com>
2019-07-24 10:49:08 -0700

    first page

--- frontend/src/views/TripInput.jsx
+++ frontend/src/views/TripInput.jsx
@@ -15,66 +15,118 @@
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
 */
-import React from "react";
+import React, { Component } from 'react';
 // nodejs library that concatenates classes
 // import classnames from "classnames";
 
 // reactstrap components
-import { FormGroup,
-  Input,
-  Container,
-  Row,
-  Button
-} from "reactstrap";
+import { FormGroup, Input, Container, Row, Button, Form, Label } from 'reactstrap';
+import axios from 'axios';
+import { geolocated } from 'react-geolocated';
+import GooglePlacesAutocomplete from 'react-google-places-autocomplete';
 
-  import TripInputTabs from "./TripInputTabs.jsx";
+import TripInputTabs from './TripInputTabs.jsx';
 
+class TripInput extends Component {
+    state = {
+        start: null,
+        destination: null,
+        optimiseFor: 'time',
+    };
 
-class TripInput extends React.Component {
-  render() {
-    return (
-      <>
-          <Container>
-            <Row className="py-3 align-items-center">
-                <h3 className="heading-title mb-0">
-                  Enter your trip
-                </h3>
-            </Row>
-            <Row className="py-3 align-items-center">
-              {/* <Col sm="3">
-                <h2>
-                  Start
-                </h2>
-              </Col> */}
-              <FormGroup>
-                  <Input placeholder="Start" type="text" />
-                </FormGroup>
-            </Row>
-            <Row className="py-3 align-items-center">
-              {/* <Col sm="3">
-                <h2>
-                  Destination
-                </h2>
-              </Col> */}
-              <FormGroup>
-                  <Input placeholder="Destination" type="text" />
-                </FormGroup>
-            </Row>
-            <Row className="py-3 align-items-center">
-              <TripInputTabs />
-            </Row>
-            <Button
-              className="ml-1 btn-neutral btn-icon-only btn-round"
-              color="success"
-              href="/recommendations"
-              size="lg"
-            >
-              Go
-            </Button>
-          </Container>
-      </>
-    );
-  }
+    handleToggle = optimiseFor => {
+        this.setState({ optimiseFor });
+    };
+
+    // placeSearch = async query => {
+    //     try {
+    //         const response = await axios.get(`http://127.0.0.1:5000/place_autocomplete/'${query}`);
+    //         console.log(response);
+    //     } catch (error) {
+    //         console.error(error);
+    //     }
+    // };
+
+    handleSubmit = async () => {
+        try {
+            const response = await axios.post('http://127.0.0.1:5000/recommendations?', {
+                dest: { lat: 11.3, long: -12222.5 },
+                start: { lat: 11.3, long: -12222.5 },
+                optimse_for: 'cost',
+            });
+            // const response = await axios.get(`http://127.0.0.1:5000/test`);
+            console.log(response);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    render() {
+        const { props } = this;
+
+        return (
+            <>
+                <Container>
+                    <Row className="py-3 align-items-center">
+                        <h3 className="heading-title mb-0">Enter your trip</h3>
+                    </Row>
+                    <Form>
+                        <FormGroup>
+                            <Label for="start">Start</Label>
+                            {/* <Input type="text" name="start" id="start" placeholder="Start" /> */}
+                            <div>
+                                <GooglePlacesAutocomplete
+                                    onSelect={({ description }) => this.setState({ address: description })}
+                                    placeholder="Starting point"
+                                />
+                            </div>
+                        </FormGroup>
+                        <FormGroup>
+                            <Label for="destination">Destination</Label>
+                            {/* <Input type="text" name="destination" id="destination" placeholder="Destination" /> */}
+                            <div>
+                                <GooglePlacesAutocomplete
+                                    onSelect={({ description }) => this.setState({ destination: description })}
+                                    placeholder="Destination"
+                                />
+                            </div>
+                        </FormGroup>
+                        <Row className="py-3 align-items-center" style={{ display: 'grid' }}>
+                            <TripInputTabs onToggle={this.handleToggle} optimiseFor={this.state.optimiseFor} />
+                        </Row>
+                        <Button color="primary" block onClick={this.handleSubmit}>
+                            Submit
+                        </Button>
+                    </Form>
+                    {/* <Row className="py-3 align-items-center">
+                                                <FormGroup>
+                                                        <Input placeholder="Start" type="text" />
+                                                </FormGroup>
+                                        </Row>
+                                        <Row className="py-3 align-items-center">
+                                                <FormGroup>
+                                                        <Input placeholder="Destination" type="text" />
+                                                </FormGroup>
+                                        </Row>
+                                        */}
+
+                    {/* <Button
+                                                className="ml-1 btn-neutral btn-icon-only btn-round"
+                                                color="success"
+                                                href="/recommendations"
+                                                size="lg"
+                                        >
+                                                Go
+                                        </Button> */}
+                </Container>
+            </>
+        );
+    }
 }
 
-export default TripInput;
+export default geolocated({
+    positionOptions: {
+        enableHighAccuracy: true,
+    },
+    userDecisionTimeout: 5000,
+})(TripInput);
